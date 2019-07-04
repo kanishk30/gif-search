72447b8b5562b59705503be2fc171b8df362c826
Kanishk Agrawal <kanishk@docon.co.in>
2019-07-04 13:24:48 +0530

    Initial working prototype of GIF Search

---
+++ src/app/gif/gif.component.ts
@@ -0,0 +1,113 @@
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  NgModule
+} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { Subject, of, fromEvent } from 'rxjs';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import {
+  debounceTime,
+  map,
+  distinctUntilChanged,
+  filter
+} from 'rxjs/operators';
+@Component({
+  selector: 'app-gif',
+  templateUrl: './gif.component.html',
+  styleUrls: ['./gif.component.scss']
+})
+export class GifComponent implements OnInit {
+  search = '';
+  imageData = [];
+  image = [];
+  isGIF = true;
+  searchResults = [];
+  fetchOne = false;
+  fetchAll = false;
+  displayResults = false;
+  searchTextChanged = new Subject<string>();
+
+  constructor(private httpClient: HttpClient) {}
+  @ViewChild('searchInput') searchInput: ElementRef;
+
+  ngOnInit() {
+    fromEvent(this.searchInput.nativeElement, 'keyup')
+      .pipe(
+        map((event: any) => event.target.value),
+        filter(result => result.length > 1),
+        debounceTime(300),
+        distinctUntilChanged()
+      )
+      .subscribe((text: string) => {
+        this.searchGetCall(text).subscribe(
+          next => {
+            // trim is used to filter out the result having no title
+            this.searchResults = next['data'].filter(e => e.title.trim());
+
+            next['data'].forEach(element => {
+              const object = { gif: '', static: '', url: '', isGIF: true };
+              object.static = element.images['480w_still'].url;
+              object.gif = element.images['downsized_medium'].url;
+              object.url = object.gif;
+              this.imageData.push(object);
+            });
+          },
+          error => {}
+        );
+      });
+  }
+
+  searchGetCall(term: string) {
+    this.imageData.length = 0;
+    this.image.length = 0;
+    if (term === '') {
+      return of([]);
+    }
+    return this.httpClient.get(
+      `http://api.giphy.com/v1/gifs/search?api_key=SqTgqUiODX49u9n09GE16RmEVuATQM2Q&q=${term}}`
+    );
+  }
+
+  searchAll() {
+    this.fetchOne = false;
+    this.fetchAll = true;
+    this.displayResults = false;
+  }
+
+  selectOne(data, index) {
+    this.image.length = 0;
+    this.fetchOne = true;
+    this.fetchAll = false;
+    this.image.push(this.imageData[index]);
+    this.toggleList();
+  }
+
+  replaceImage(index, source) {
+    let arr = [];
+    arr = source === 'one' ? [...this.image] : [...this.imageData];
+    const tempURL = arr[index]['static'];
+    const originalURL = arr[index]['gif'];
+    if (arr[index].isGIF) {
+      arr[index]['url'] = tempURL;
+      arr[index].isGIF = false;
+    } else {
+      arr[index]['url'] = originalURL;
+      arr[index].isGIF = true;
+    }
+  }
+
+  toggleList() {
+    if (this.search) {
+      this.displayResults = !this.displayResults;
+    }
+  }
+
+  searching() {
+    this.displayResults = true;
+  }
+}
